d81671d12c851bd726947c810579f0a4f930a6b4
Amanda Kirchner <geeky.brewster@gmail.com>
2021-06-20 12:56:23 -0500

    Added 2nd useEffect to fix warning & prevent bug

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,6 +6,7 @@ const Search = () => {
     const baseWikipediaUrl = "https://en.wikipedia.org/w/api.php";
 
     const [searchTerm, setSearchTerm] = useState('llama');
+    const [debouncedTerm, setDebouncedTerm] = useState(searchTerm);
     const [results, setResults] = useState([]);
 
     //useEffect is passed two arguments - the first is the function to run at render
@@ -14,6 +15,21 @@ const Search = () => {
         //nothing - run function at initial render and at every re-render
         //an array w/ data - run at initial render and at every re-render if one of the element in the array has changed
 
+    //Going to use this one to monitor user input and apply wait before sending search term to the request logic
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(searchTerm);
+        }, 1000);
+
+        //useEffect can return one thing, a function - good to use for cleanup
+        //This will be called BEFORE the above function any time the component re-renders (not called on initial render)
+        return () => {
+            clearTimeout(timerId);
+        };
+
+    }, [searchTerm]);
+
+    //Going to use this one to make the API requests
     useEffect (() => {
         //have to put async-await inside the function here due to useEffect not wanting async function as first param
         const search = async () => {
@@ -23,31 +39,23 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: searchTerm
+                    srsearch: debouncedTerm
                 }
             });
 
             setResults(data.query.search);
         };
 
-        if(searchTerm && !results.length){
-            search();
+        search();
+ 
+    }, [debouncedTerm]); //dependency array (ideally useEffect want all references to state items in here)
 
-        } else {
-            const timeoutId = setTimeout(() => {
-                if(searchTerm){
-                    search();
-                }
-            }, 500);
-    
-            //useEffect can return one thing, a function - good to use for cleanup
-            //This will be called BEFORE the above function any time the component re-renders (not called on initial render)
-            return () => {
-                clearTimeout(timeoutId);
-            };
-        }
-      
-    }, [searchTerm]);
+    //Previously we had one useEffect that set the timeout, 
+    //but also had a check on results.length so we didn't use the timeout for initial render/request
+    //But having both searchTerm and results in the dependency array, you end up with additional API requests, which we don't want,
+    //due to change in results after initial render triggering another render and a new request
+    //So to fix we separated that out into separate useEffects targeting different pieces of state.
+    //Bonus - if search term is re-entered but the same, no new request is made
 
 
     //There is a hidden React function that converts a string into HTML, but I don't wish to use it [Trust No One]
